67c21cf7378bd97f63fc1d803da95bb1d396bfb1
ellafinawres <nawres.ellafi@esprit.tn>
2022-11-03 11:53:06 +0100

    First commit evenement.h

---
+++ evenement.h
@@ -0,0 +1,44 @@
+#ifndef EVENEMENT_H
+#define EVENEMENT_H
+#include<QString>
+#include <QDateEdit>
+//#include <QTimeEdit>
+#include<QSqlQuery>
+#include<QSqlQueryModel>
+class evenement
+{   
+private:
+    QString type;
+    int numero,numero_salle,numero_participants,id_personnel;
+    QDate date;
+    QTime duree;
+    float tarif;
+public:
+    evenement();
+     evenement(QString,int,int,int,int,float,QDate,QTime);
+    void setnumero(int n);
+    void settype(QString n);
+    void setdate(QDate n);
+    void setid_personnel(int n);
+    void setnumero_salle(int n);
+    void setduree(QTime n);
+    void setnumero_participants(int n);
+    void settarif(float n);
+
+
+    int get_numero();
+    QString get_type();
+    QDate get_date();
+    int  get_id_personnel();
+    int get_numero_salle();
+    QTime get_duree();
+    int get_numero_participants();
+    float get_tarif();
+bool ajouter();
+QSqlQueryModel * afficher();
+bool supprimer(int);
+ bool modifier(int,QString,QDate,QTime,float,int,int,int);
+
+};
+
+#endif // EVENEMENT_H
